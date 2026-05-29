f824adbd422ee769ddd5e0b6ef635aa6aae4e757
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-29 15:50:03 +0300

    layouts for all the pages

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/home/Footer";
+import NavbarSection from "@/components/Navbarsection";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <NavbarSection />
+      <body className="min-h-full flex flex-col">{children}</body>
+      <Footer />
+    </html>
+  );
+}
